0dfe6bdf9316b75d3c5f20acbc9aeb5829219d94
Jamesburgess44 <jamesburgess44@yahoo.com>
2021-08-20 14:00:31 -0600

    added context for contact page, needs style

--- src/Components/Home/Contact/contact.js
+++ src/Components/Home/Contact/contact.js
@@ -4,7 +4,13 @@ export default class Contact extends Component {
     render() {
         return (
             <div>
-                <h1>Contact</h1>
+                <h1>Contact Details</h1>
+                <br></br>
+                <h2>Email Address</h2>
+                <h2>danipaskevitch@gmail.com</h2>
+                <br></br>
+                <h2>Phone Number</h2>
+                <h2>(801)201-6799</h2>
             </div>
         )
     }
