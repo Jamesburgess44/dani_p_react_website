591cc524f909ba6bfd470423cb6d5126bc0003ba
Jamesburgess44 <jamesburgess44@yahoo.com>
2021-08-20 10:46:36 -0600

    setup framework for navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './App.css';
+import Navbar from './Components/Navbar/navbar';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello Dani Paskevitch</h1>
-    </div>
+    <>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path='/'exact />
+      </Switch>
+    </Router>
+    </>
   );
 }
 
