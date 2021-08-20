e52d18effb9b1095a3ae4d23e185783dfff064af
Jamesburgess44 <jamesburgess44@yahoo.com>
2021-08-20 13:54:44 -0600

    added text to about

--- src/Components/About/about.js
+++ src/Components/About/about.js
@@ -1,11 +1,18 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class about extends Component {
-    render() {
-        return (
-            <div>
-                <h1>About</h1>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <h1>Get To Know Dani</h1>
+        <br></br>
+        <h2>
+          I live, love, and breathe business and creativity. I am constantly
+          thinking of new and innovative ideas or looking at things in an
+          inspired light. I am always up to date on the latest marketing
+          techniques and scroll-stopping trends.
+        </h2>
+      </div>
+    );
+  }
 }
