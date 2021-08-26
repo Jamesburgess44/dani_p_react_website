21b83dfd70a35aaa3aecb7e890285bcde39e49ef
Jamesburgess44 <jamesburgess44@yahoo.com>
2021-08-26 09:32:32 -0600

    revmoved footer, replacing with sticky navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavMenu from './Components/Navbar/navMenu';
-import Footer from './Components/Footer/footer';
 
 
 function App() {
   return (
   <div className="App">
     <NavMenu />
-    <Footer />
   </div>
   
   );
