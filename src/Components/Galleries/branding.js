9635eaec54aa1ab3de18554246df9d1e98e18c96
Jamesburgess44 <jamesburgess44@yahoo.com>
2021-08-29 13:41:00 -0600

    removed background and layers from branding component

--- src/Components/Galleries/branding.js
+++ src/Components/Galleries/branding.js
@@ -1,53 +1,42 @@
-import React from "react";
-import Box from "@material-ui/core/Box";
-import { makeStyles } from "@material-ui/core/styles";
+import React, { useState, useCallback } from "react";
 import { BrandingPhotos } from "./brandingPhotos";
 import Gallery from "react-photo-gallery";
+import Carousel, { Modal, ModalGateway } from "react-images";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    width: "100%",
-    height: "85%",
-    position: "relative", "& video": {
-    objectFit: "cover",
-    },
-  },
-  overlay: {
-    position: "absolute",
-    top: 0,
-    left: 0,
-    width: "100%",
-    height: "100%",
-    backgroundColor: "rgba(0, 0, 0, 0.5)",
-  },
-  title: {
-    paddingBottom: theme.spacing(4),
-  },
-}));
 
-const Branding = () => {
-  const classes = useStyles();
-
-  return (
-    <section className={classes.root}>
-        <img src="/images/background.webp" 
-        height= "100%"
-        width= "100%"
-        />
-      <div className={classes.overlay}>
-      <Gallery photos={BrandingPhotos} direction={"column"} />
-        <Box
-          height="100%"
-          display="flex"
-          flexDirection="column"
-          justifyContent="top"
-          alignItems="center"
-          color="#fff"
-        >
-        </Box>
+function Branding() {
+    const [currentImage, setCurrentImage] = useState(0);
+    const [viewerIsOpen, setViewerIsOpen] = useState(false);
+  
+    const openLightbox = useCallback((event, { photo, index }) => {
+      setCurrentImage(index);
+      setViewerIsOpen(true);
+    }, []);
+  
+    const closeLightbox = () => {
+      setCurrentImage(0);
+      setViewerIsOpen(false);
+    };
+  
+    return (
+      <div>
+        <Gallery photos={BrandingPhotos} onClick={openLightbox} />
+        <ModalGateway>
+          {viewerIsOpen ? (
+            <Modal onClose={closeLightbox}>
+              <Carousel
+                currentIndex={currentImage}
+                views={BrandingPhotos.map(x => ({
+                  ...x,
+                  srcset: x.srcSet,
+                  caption: x.title
+                }))}
+              />
+            </Modal>
+          ) : null}
+        </ModalGateway>
       </div>
-    </section>
-  );
-};
+    );
+  }
 
-export default Branding;
+  export default Branding;
